a368fbf15a4dd9b827cff30210603e106ac4966a
Matt Jackson <mw.jackson@hotmail.com>
2010-02-18 21:28:12 +0000

    - changed fsunit to plain old nunit

---
+++ LondonTransportStatusTests/NewTubeStatusTests.fs
@@ -0,0 +1,26 @@
+﻿namespace LondonTransportStatus.Tests
+
+open NUnit.Framework
+open System.Xml
+
+type TubeStatus(lines)=
+
+    let lines = lines
+    member x.Lines = lines
+    member x.Status = []
+
+[<TestFixture>]
+type TubeStatusTests ()=
+
+    let lines = ["northern"]
+
+    [<Test>] 
+    member x.should_take_a_list_of_tube_lines() =
+        let tubeStatus = new TubeStatus(lines)
+        Assert.That(tubeStatus.Lines, Is.EqualTo(lines))
+
+    [<Test>]
+    member x.status_on_no_lines_should_return_empty_list() =
+        let tubeStatus = new TubeStatus([])
+        Assert.That(tubeStatus.Status, Is.EqualTo([]))
+
