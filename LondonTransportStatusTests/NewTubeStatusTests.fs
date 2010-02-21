2965ee4878ca037b3e5121b2e79522222f689f7d
Matt Jackson <mw.jackson@hotmail.com>
2010-02-21 23:17:04 +0000

    - figured out how to use rhino mocks and interfaces

--- LondonTransportStatusTests/NewTubeStatusTests.fs
+++ LondonTransportStatusTests/NewTubeStatusTests.fs
@@ -1,13 +1,36 @@
 ﻿namespace LondonTransportStatus.Tests
 
 open NUnit.Framework
+open Rhino.Mocks
 open System.Xml
+open System
 
-type TubeStatus(lines)=
+type ITflQuery =
+    abstract member Query : unit -> unit
 
-    let lines = lines
-    member x.Lines = lines
-    member x.Status = []
+type TflQuery() =
+    interface ITflQuery with
+        member this.Query() =
+            Console.WriteLine("querying tfl")
+
+type ITubeStatus =
+    abstract member Status : unit -> list<string>
+
+type TubeStatus(tflQuery:ITflQuery, lines) =
+    
+    let _tflQuery = tflQuery
+
+    let _lines = lines
+    member this.Lines = _lines
+
+    interface ITubeStatus with
+        member this.Status() = 
+            match(lines) with
+            | [] | [""] -> []
+            | _ -> 
+                _tflQuery.Query()
+                []
+    end
 
 [<TestFixture>]
 type TubeStatusTests ()=
@@ -16,11 +39,29 @@ type TubeStatusTests ()=
 
     [<Test>] 
     member x.should_take_a_list_of_tube_lines() =
-        let tubeStatus = new TubeStatus(lines)
+        let tubeStatus = new TubeStatus(new TflQuery(), lines)
         Assert.That(tubeStatus.Lines, Is.EqualTo(lines))
 
     [<Test>]
     member x.status_on_no_lines_should_return_empty_list() =
-        let tubeStatus = new TubeStatus([])
-        Assert.That(tubeStatus.Status, Is.EqualTo([]))
+        let tubeStatus = new TubeStatus(new TflQuery(), [])
+        let status = (tubeStatus :> ITubeStatus).Status()
+        Assert.That(status, Is.EqualTo([]))
 
+    [<Test>]
+    member x.status_on_a_single_line_should_return_list_with_a_string() =
+        let tubeStatus = new TubeStatus(new TflQuery(), ["aLine"])
+        let status = (tubeStatus :> ITubeStatus).Status()
+        Assert.That(status, Is.TypeOf(typeof<list<string>>))
+
+    [<Test>]
+    member x.status_should_query_a_tube_status_document() =
+        let tflQuery = MockRepository.GenerateStub<ITflQuery>();
+
+        let lines = ["northern"]
+        let tubeStatus = new TubeStatus(tflQuery, lines)
+        
+        let result = (tubeStatus :> ITubeStatus).Status()
+
+        RhinoMocksExtensions.AssertWasCalled<ITflQuery>(tflQuery, fun x -> (x :> ITflQuery).Query())
+        
